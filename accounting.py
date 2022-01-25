9aaebb009976325ed5e1ee757eccfdfc4a5ca2f3
Ta-nu-ki <pavbulgakov@gmail.com>
2022-01-26 00:49:48 +0300

    typo fix

--- accounting.py
+++ accounting.py
@@ -1,6 +1,6 @@
 def margin_to_markup_conversion(margin):
-    return margin * (1 - margin)
+    return margin / (1 - margin)
 
 
 def markup_to_margin_conversion(markup):
-    return markup * (1 + markup)
+    return markup / (1 + markup)
