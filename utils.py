474cb8c56c05d31545120872b3e0704829568a0c
Ta-nu-ki <pavbulgakov@gmail.com>
2022-01-28 01:07:52 +0300

    Added stopwatch decorator to utils.py file

---
+++ utils.py
@@ -0,0 +1,14 @@
+from functools import wraps
+from time import time
+
+
+def stopwatch(f):
+    """Measures function execution time"""
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        ts = time()
+        result = f(*args, **kwargs)
+        te = time()
+        print(f"'{f.__name__}' function execution time is {(te-ts):.4f} sec")
+        return result
+    return wrap
